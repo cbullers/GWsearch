6b042585b971e4722c7074ef76430c9e220a0856
Christian Bullers <chris@bullers.us>
2023-08-27 19:19:43 -0500

    update destinations include tz on clientside

--- gw-client/src/destinations.ts
+++ gw-client/src/destinations.ts
@@ -1,99 +1,386 @@
 export default {
-    "ANU": "Antigua and Barbuda", 
-    "NAS": "Bahamas", 
-    "BZE": "Belize", 
-    "LIR": "Costa Rica", 
-    "SJO": "San José", 
-    "PUJ": "Punta Cana, DR", 
-    "SDQ": "Santo Domingo, DR", 
-    "SAL": "El Salvador", 
-    "GUA": "Guatemala", 
-    "KIN": "Jamaica", 
-    "MBJ": "St. James", 
-    "SJD": "Los Cabos, MX", 
-    "GDL": "Guadalajara, MX", 
-    "PVR": "Puerto Vallarta, MX", 
-    "MTY": "Monetrrey, MX", 
-    "CUN": "Cancun, MX", 
-    "CZM": "Cozumel, MX",  
-    "SXM": "St. Maarten", 
-    "BQN": "Aguadilla, Puerto Rico", 
-    "PSE": "Ponce, Puerto Rico", 
-    "SJU": "San Juan, Puerto Rico",
-    "PHX": "Phoenix", 
-    "XNA": "Arkansas", 
-    "LIT": "Little Rock, AR", 
-    "OAK": "Oakland", 
-    "ONT": "Ontario", 
-    "SNA": "Orange County", 
-    "SMF": "Sacramento", 
-    "SAN": "San Diego", 
-    "SFO": "San Francisco", 
-    "DEN": "Colorado", 
-    "BDL": "Connecticut", 
-    "FLL": "Fort Lauderdale, FL", 
-    "RSW": "Fort Myers, FL", 
-    "JAX": "Jacksonville, FL", 
-    "MIA": "Miami, FL", 
-    "MCO": "Orlando, FL", 
-    "PNS": "Pensacola, FL", 
-    "SRQ": "Sarasota, FL", 
-    "TPA": "Tampa, FL", 
-    "PBI": "West Palm Beach, FL", 
-    "ATL": "Atlanta, Georgia", 
-    "SAV": "Savannah, Georgia", 
-    "BMI": "Illinois", 
-    "MDW": "Chicago",
-    "ORD": "Chicago", 
-    "IND": "Indiana", 
-    "CID": "Cedar rapids, Iowa", 
-    "DSM": "Des Moines, Iowa", 
-    "CVG": "Kentucky", 
-    "MSY": "Louisiana", 
-    "PWM": "Maine", 
-    "BWI": "Maryland", 
-    "BOS": "Massachusetts", 
-    "DTW": "Michigan", 
-    "GRR": "Grand Rapids, MI", 
-    "MSP": "Minnesota", 
-    "MCI": "Missouri", 
-    "STL": "St. Louis", 
-    "MSO": "Montana", 
-    "OMA": "Nebraska", 
-    "LAS": "Las Vegas", 
-    "TTN": "New Jersey", 
-    "BUF": "New York", 
-    "ISP": "Long Island/Islip", 
-    "SWF": "Newburgh", 
-    "LGA": "New York City", 
-    "SYR": "Syracuse", 
-    "CLT": "North Carolina", 
-    "RDU": "Raleigh, NC", 
-    "FAR": "North Dakota", 
-    "CLE": "Ohio", 
-    "CMH": "Columbus", 
-    "OKC": "Oklahoma", 
-    "PDX": "Oregon", 
-    "MDT": "Pennsylvania", 
-    "PHL": "Philadelphia", 
-    "PIT": "Pittsburgh", 
-    "CHS": "Charleston, South Carolina", 
-    "MYR": "Myrtle Beach, SC", 
-    "TYS": "Tennessee", 
-    "MEM": "Memphis", 
-    "BNA": "Nashville", 
-    "AUS": "Austin, Texas", 
-    "DFW": "Dallas/Fort Worth", 
-    "ELP": "El Paso", 
-    "IAH": "Houston",
-    "HOU": "Houston", 
-    "SAT": "San Antonio", 
-    "STT": "U.S. Virgin Islands", 
-    "SLC": "Utah", 
-    "DCA": "Virginia", 
-    "ORF": "Norfolk", 
-    "SEA": "Washington", 
-    "GRB": "Wisconsin", 
-    "MSN": "Madison", 
-    "MKE": "Milwaukee"
+    "NAS": {
+        "desc": "Bahamas",
+        "tz": "America/Nassau"
+    },
+    "BZE": {
+        "desc": "Belize",
+        "tz": "America/Belize"
+    },
+    "LIR": {
+        "desc": "Costa Rica",
+        "tz": "America/Costa_Rica"
+    },
+    "SJO": {
+        "desc": "San Jos",
+        "tz": "America/Costa_Rica"
+    },
+    "PUJ": {
+        "desc": "Punta Cana, DR",
+        "tz": "America/Santo_Domingo"
+    },
+    "SDQ": {
+        "desc": "Santo Domingo, DR",
+        "tz": "America/Santo_Domingo"
+    },
+    "SAL": {
+        "desc": "El Salvador",
+        "tz": "America/El_Salvador"
+    },
+    "GUA": {
+        "desc": "Guatemala",
+        "tz": "America/Guatemala"
+    },
+    "KIN": {
+        "desc": "Jamaica",
+        "tz": "America/Jamaica"
+    },
+    "MBJ": {
+        "desc": "St. James",
+        "tz": "America/Jamaica"
+    },
+    "SJD": {
+        "desc": "Los Cabos, MX",
+        "tz": "America/Mazatlan"
+    },
+    "GDL": {
+        "desc": "Guadalajara, MX",
+        "tz": "America/Mexico_City"
+    },
+    "PVR": {
+        "desc": "Puerto Vallarta, MX",
+        "tz": "America/Mexico_City"
+    },
+    "MTY": {
+        "desc": "Monetrrey, MX",
+        "tz": "America/Mexico_City"
+    },
+    "CUN": {
+        "desc": "Cancun, MX",
+        "tz": "America/Cancun"
+    },
+    "CZM": {
+        "desc": "Cozumel, MX",
+        "tz": "America/Cancun"
+    },
+    "SXM": {
+        "desc": "St. Maarten",
+        "tz": "America/Curacao"
+    },
+    "BQN": {
+        "desc": "Aguadilla, Puerto Rico",
+        "tz": "America/Puerto_Rico"
+    },
+    "PSE": {
+        "desc": "Ponce, Puerto Rico",
+        "tz": "America/Puerto_Rico"
+    },
+    "SJU": {
+        "desc": "San Juan, Puerto Rico",
+        "tz": "America/Puerto_Rico"
+    },
+    "PHX": {
+        "desc": "Phoenix",
+        "tz": "America/Phoenix"
+    },
+    "XNA": {
+        "desc": "Arkansas",
+        "tz": "America/Chicago"
+    },
+    "LIT": {
+        "desc": "Little Rock, AR",
+        "tz": "America/Chicago"
+    },
+    "OAK": {
+        "desc": "Oakland",
+        "tz": "America/Los_Angeles"
+    },
+    "ONT": {
+        "desc": "Ontario",
+        "tz": "America/Los_Angeles"
+    },
+    "SNA": {
+        "desc": "Orange County",
+        "tz": "America/Los_Angeles"
+    },
+    "SMF": {
+        "desc": "Sacramento",
+        "tz": "America/Los_Angeles"
+    },
+    "SAN": {
+        "desc": "San Diego",
+        "tz": "America/Los_Angeles"
+    },
+    "SFO": {
+        "desc": "San Francisco",
+        "tz": "America/Los_Angeles"
+    },
+    "DEN": {
+        "desc": "Colorado",
+        "tz": "America/Denver"
+    },
+    "BDL": {
+        "desc": "Connecticut",
+        "tz": "America/New_York"
+    },
+    "FLL": {
+        "desc": "Fort Lauderdale, FL",
+        "tz": "America/New_York"
+    },
+    "RSW": {
+        "desc": "Fort Myers, FL",
+        "tz": "America/New_York"
+    },
+    "JAX": {
+        "desc": "Jacksonville, FL",
+        "tz": "America/New_York"
+    },
+    "MIA": {
+        "desc": "Miami, FL",
+        "tz": "America/New_York"
+    },
+    "MCO": {
+        "desc": "Orlando, FL",
+        "tz": "America/New_York"
+    },
+    "PNS": {
+        "desc": "Pensacola, FL",
+        "tz": "America/Chicago"
+    },
+    "SRQ": {
+        "desc": "Sarasota, FL",
+        "tz": "America/New_York"
+    },
+    "TPA": {
+        "desc": "Tampa, FL",
+        "tz": "America/New_York"
+    },
+    "PBI": {
+        "desc": "West Palm Beach, FL",
+        "tz": "America/New_York"
+    },
+    "ATL": {
+        "desc": "Atlanta, Georgia",
+        "tz": "America/New_York"
+    },
+    "SAV": {
+        "desc": "Savannah, Georgia",
+        "tz": "America/New_York"
+    },
+    "BMI": {
+        "desc": "Illinois",
+        "tz": "America/Chicago"
+    },
+    "MDW": {
+        "desc": "Chicago",
+        "tz": "America/Chicago"
+    },
+    "ORD": {
+        "desc": "Chicago",
+        "tz": "America/Chicago"
+    },
+    "IND": {
+        "desc": "Indiana",
+        "tz": "America/New_York"
+    },
+    "CID": {
+        "desc": "Cedar rapids, Iowa",
+        "tz": "America/Chicago"
+    },
+    "DSM": {
+        "desc": "Des Moines, Iowa",
+        "tz": "America/Chicago"
+    },
+    "CVG": {
+        "desc": "Kentucky",
+        "tz": "America/New_York"
+    },
+    "MSY": {
+        "desc": "Louisiana",
+        "tz": "America/Chicago"
+    },
+    "PWM": {
+        "desc": "Maine",
+        "tz": "America/New_York"
+    },
+    "BWI": {
+        "desc": "Maryland",
+        "tz": "America/New_York"
+    },
+    "BOS": {
+        "desc": "Massachusetts",
+        "tz": "America/New_York"
+    },
+    "DTW": {
+        "desc": "Michigan",
+        "tz": "America/New_York"
+    },
+    "GRR": {
+        "desc": "Grand Rapids, MI",
+        "tz": "America/New_York"
+    },
+    "MSP": {
+        "desc": "Minnesota",
+        "tz": "America/Chicago"
+    },
+    "MCI": {
+        "desc": "Missouri",
+        "tz": "America/Chicago"
+    },
+    "STL": {
+        "desc": "St. Louis",
+        "tz": "America/Chicago"
+    },
+    "MSO": {
+        "desc": "Montana",
+        "tz": "America/Denver"
+    },
+    "OMA": {
+        "desc": "Nebraska",
+        "tz": "America/Chicago"
+    },
+    "LAS": {
+        "desc": "Las Vegas",
+        "tz": "America/Los_Angeles"
+    },
+    "TTN": {
+        "desc": "New Jersey",
+        "tz": "America/New_York"
+    },
+    "BUF": {
+        "desc": "New York",
+        "tz": "America/New_York"
+    },
+    "ISP": {
+        "desc": "Long Island/Islip",
+        "tz": "America/New_York"
+    },
+    "SWF": {
+        "desc": "Newburgh",
+        "tz": "America/New_York"
+    },
+    "LGA": {
+        "desc": "New York City",
+        "tz": "America/New_York"
+    },
+    "SYR": {
+        "desc": "Syracuse",
+        "tz": "America/New_York"
+    },
+    "CLT": {
+        "desc": "North Carolina",
+        "tz": "America/New_York"
+    },
+    "RDU": {
+        "desc": "Raleigh, NC",
+        "tz": "America/New_York"
+    },
+    "FAR": {
+        "desc": "North Dakota",
+        "tz": "America/Chicago"
+    },
+    "CLE": {
+        "desc": "Ohio",
+        "tz": "America/New_York"
+    },
+    "CMH": {
+        "desc": "Columbus",
+        "tz": "America/New_York"
+    },
+    "OKC": {
+        "desc": "Oklahoma",
+        "tz": "America/Chicago"
+    },
+    "PDX": {
+        "desc": "Oregon",
+        "tz": "America/Los_Angeles"
+    },
+    "MDT": {
+        "desc": "Pennsylvania",
+        "tz": "America/New_York"
+    },
+    "PHL": {
+        "desc": "Philadelphia",
+        "tz": "America/New_York"
+    },
+    "PIT": {
+        "desc": "Pittsburgh",
+        "tz": "America/New_York"
+    },
+    "CHS": {
+        "desc": "Charleston, South Carolina",
+        "tz": "America/New_York"
+    },
+    "MYR": {
+        "desc": "Myrtle Beach, SC",
+        "tz": "America/New_York"
+    },
+    "TYS": {
+        "desc": "Tennessee",
+        "tz": "America/New_York"
+    },
+    "MEM": {
+        "desc": "Memphis",
+        "tz": "America/Chicago"
+    },
+    "BNA": {
+        "desc": "Nashville",
+        "tz": "America/Chicago"
+    },
+    "AUS": {
+        "desc": "Austin, Texas",
+        "tz": "America/Chicago"
+    },
+    "DFW": {
+        "desc": "Dallas/Fort Worth",
+        "tz": "America/Chicago"
+    },
+    "ELP": {
+        "desc": "El Paso",
+        "tz": "America/Denver"
+    },
+    "IAH": {
+        "desc": "Houston",
+        "tz": "America/Chicago"
+    },
+    "HOU": {
+        "desc": "Houston",
+        "tz": "America/Chicago"
+    },
+    "SAT": {
+        "desc": "San Antonio",
+        "tz": "America/Chicago"
+    },
+    "STT": {
+        "desc": "U.S. Virgin Islands",
+        "tz": "America/St_Thomas"
+    },
+    "SLC": {
+        "desc": "Utah",
+        "tz": "America/Denver"
+    },
+    "DCA": {
+        "desc": "Virginia",
+        "tz": "America/New_York"
+    },
+    "ORF": {
+        "desc": "Norfolk",
+        "tz": "America/New_York"
+    },
+    "SEA": {
+        "desc": "Washington",
+        "tz": "America/Los_Angeles"
+    },
+    "GRB": {
+        "desc": "Wisconsin",
+        "tz": "America/Chicago"
+    },
+    "MSN": {
+        "desc": "Madison",
+        "tz": "America/Chicago"
+    },
+    "MKE": {
+        "desc": "Milwaukee",
+        "tz": "America/Chicago"
+    }
 }
